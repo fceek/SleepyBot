de591a393b76322f4f10068e2dfd7f425e447c08
fceek <fceek96@gmail.com>
2022-01-11 01:21:10 +0800

    wanna go home, wanna go vacation!

---
+++ SleepyBot/Modules/Underwork/UnderworkRouter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Maila.Cocoa.Framework;
+using SleepyBot.Utilities;
+
+namespace SleepyBot.Modules.Underwork
+{
+    [BotModule]
+    public class UnderworkModule : BotModuleBase
+    {
+        [RegexRoute("(.*)过年放假(.*)")]
+        public static string GetTimeTillNewYear(MessageSource src)
+        {
+            string response = Time.FromNowToTargetStr(new DateTime(2022, 1, 26, 19, 0, 0, DateTimeKind.Local));
+            return $"离过年放假还有{response}！";
+        }
+
+        [RegexRoute("(.*)下班(.*)")]
+        public static string GetTimeTillOffWork(MessageSource src)
+        {
+            DateTime offWorkTime = DateTime.Today.AddHours(19);
+            if (DateTime.Compare(offWorkTime, DateTime.Now) < 0)
+            {
+                return "已经下班了！";
+            }
+
+            string response = Time.FromNowToTargetStr(offWorkTime);
+            return $"离下班还有{response}！";
+        }
+    }
+}
---
+++ SleepyBot/Utilities/Time.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SleepyBot.Utilities
+{
+    public static class Time
+    {
+        public static string FromNowToTargetStr(DateTime target)
+        {
+            TimeSpan span = target - DateTime.Now;
+            string response = string.Empty;
+            if (span.Days >= 1)
+            {
+                response += span.Days + "天";
+            }
+
+            response += $"{span.Hours}小时{span.Minutes}分{span.Seconds}秒";
+            return response;
+        }
+    }
+}
